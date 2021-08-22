98757e8672f01b176d592b2634fca5632c550664
rcmdnk <rcmdnk@gmail.com>
2021-08-23 05:48:11 +0900

    change docker to composite

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,24 +9,31 @@ execute () {
   echo "########################################"
   "$@"
 }
+generate_option=$1
+ssh_key=$2
+known_hosts=$3
+git_user_email=$4
+git_user_name=$5
+deploy_option=$6
 
 execute bundle install
+execute gem update rake
 execute rake integrate
 
-if [ "$INPUT_MINIFY" = "1" ] || [ "${INPUT_MINIFY^^}" = "TRUE" ];then
+if [ "$generate_option" = "1" ] || [ "${generate_option^^}" = "TRUE" ];then
   execute rake generate --trace
 else
   execute rake generate['no_minify'] --trace
 fi
 
-execute mkdir -p /root/.ssh
-execute chmod 700 /root/.ssh
-echo "$INPUT_SSH_KEY" > /root/.ssh/id_rsa
-execute chmod 600 /root/.ssh/id_rsa
-execute echo "$INPUT_KNOWN_HOSTS" > /root/.ssh/known_hosts
-execute chmod 600 /root/.ssh/known_hosts
+execute mkdir -p ~/.ssh
+execute chmod 700 ~/.ssh
+echo "$ssh_key" > ~/.ssh/id_rsa
+execute chmod 600 ~/.ssh/id_rsa
+execute echo "$known_hosts" > ~/.ssh/known_hosts
+execute chmod 600 ~/.ssh/known_hosts
 
-execute git config --global user.email "$INPUT_GIT_USER_EMAIL"
-execute git config --global user.name "$INPUT_GIT_USER_NAME"
+execute git config --global user.email "$git_user_email"
+execute git config --global user.name "$git_user_name"
 
-execute rake deploy["$INPUT_DEPLOY_OPTION"]
+execute rake deploy["$deploy_option"]
